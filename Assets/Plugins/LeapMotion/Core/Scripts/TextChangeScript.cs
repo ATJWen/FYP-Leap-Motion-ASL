076f1518f155883952e6e8a9d4c311ba42a4c600
ATJWen <atjwen@outlook.my>
2021-03-13 15:08:05 +0000

    first attempt at image recognition

---
+++ Assets/Plugins/LeapMotion/Core/Scripts/TextChangeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChangeScript : MonoBehaviour
+{
+    public TextMeshPro predicted_word;
+    public string temp_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        predicted_word = GetComponent<TextMeshPro> ();
+        temp_text = "Left";
+        predicted_word.text = temp_text;
+        Debug.Log(Application.dataPath);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
